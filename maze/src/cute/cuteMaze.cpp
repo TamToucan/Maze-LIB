302571d7e84016e57424ce717ed8624280da2a6e
Tam Toucan <dr@tamtoucan.com>
2025-12-23 12:57:31 +0000

    Change case of dll name

--- maze/src/cute/cuteMaze.cpp
+++ maze/src/cute/cuteMaze.cpp
@@ -1,23 +1,22 @@
-#include <cute.h>
-#include "MazeHelper.h"
 #include "MazeData.h"
+#include "MazeHelper.h"
+#include <cute.h>
 
 // This class is just a wrapper to prove both libraries can talk to each other
 class CuteMaze {
 
 public:
-    CuteMaze(int width, int height) {
-	    pMaze = Maze::MazeHelper::generateSquareMaze(width, height, 0, 0, false, false, false, 50, 4242);
-    }
-    ~CuteMaze() {
-	    delete pMaze;
-    }
+  CuteMaze(int width, int height) {
+    pMaze = Maze::MazeHelper::generateSquareMaze(width, height, 0, 0, false,
+                                                 false, false, 50, 4242);
+  }
+  ~CuteMaze() { delete pMaze; }
 
-    void draw() {
-	    Cute::draw_push_color(Cute::color_white());
-	    Cute::draw_line(Cute::v2(0,0), Cute::v2(100, 100), 2.0f);
-    }
+  void draw() {
+    Cute::draw_push_color(Cute::color_white());
+    Cute::draw_line(cf_v2(0, 0), cf_v2(100, 100), 2.0f);
+  }
 
 private:
-    Maze::MazeData* pMaze;
+  Maze::MazeData *pMaze;
 };
