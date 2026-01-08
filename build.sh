013675693fac4276db95e00181542278dc9ef95d
Tam Toucan <dr@tamtoucan.com>
2026-01-08 22:21:53 +0000

    Fixup cute building

--- build.sh
+++ build.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
-cmake --preset=mingw64 -S . -B build -DBUILD_GODOT=OFF -DBUILD_CUTE=ON
-cmake --build build
+opt=""
+target=""
+
+[[ -z "$1" ]] && echo "$0 <core | godot | cute | all>" && exit 1
+
+[[ $1 == "core" ]] &&
+opt="-DBUILD_GODOT=OFF -DBUILD_CUTE=OFF" 
+
+[[ $1 == "godot" ]] &&
+opt="-DBUILD_GODOT=ON -DBUILD_CUTE=OFF" 
+
+[[ $1 == "cute" ]] &&
+opt="-DBUILD_GODOT=OFF -DBUILD_CUTE=ON" 
+
+[[ $1 == "all" ]] &&
+opt="-DBUILD_GODOT=ON -DBUILD_CUTE=ON" 
+
+cmake --preset=mingw64 -S . -B build $opt
+cmake --build build 
 
