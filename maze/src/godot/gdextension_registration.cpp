926014d7b1c1e1afcaf4241ffb40de4ad77a5d7c
Tam Toucan <dr@tamtoucan.com>
2026-01-18 23:26:45 +0000

    Remove ALL debug

--- maze/src/godot/gdextension_registration.cpp
+++ maze/src/godot/gdextension_registration.cpp
@@ -1,41 +1,41 @@
 #include "gdextension_registration.hpp"
 
 #include <gdextension_interface.h>
+
 #include <godot_cpp/core/defs.hpp>
 #include <godot_cpp/godot.hpp>
-#include "GDMaze.hpp"
 
 #include "Debug.h"
+#include "GDMaze.hpp"
+
 
 using namespace godot;
 
 void initialize_libgdmaze(ModuleInitializationLevel p_level) {
-	if (p_level != MODULE_INITIALIZATION_LEVEL_CORE) {
-		return;
-	}
-	SET_DEBUG("ALL");
-	LOG_INFO("################# REGISTER GDMaze");
-	ClassDB::register_class<GDMaze>();
+  if (p_level != MODULE_INITIALIZATION_LEVEL_CORE) {
+    return;
+  }
+  LOG_INFO("################# REGISTER GDMaze");
+  ClassDB::register_class<GDMaze>();
 }
 
 void uninitialize_libgdmaze(ModuleInitializationLevel p_level) {
-	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
-		return;
-	}
+  if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+    return;
+  }
 }
 
 extern "C" {
 
 GDExtensionBool GDE_EXPORT libgdmaze_init(GDExtensionInterfaceGetProcAddress p_get_proc_address,
-										       const GDExtensionClassLibraryPtr p_library,
-		                                       GDExtensionInitialization *r_initialization) {
-	godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
+                                          const GDExtensionClassLibraryPtr p_library,
+                                          GDExtensionInitialization* r_initialization) {
+  godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
 
-	init_obj.register_initializer(initialize_libgdmaze);
-	init_obj.register_terminator(uninitialize_libgdmaze);
-	init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
+  init_obj.register_initializer(initialize_libgdmaze);
+  init_obj.register_terminator(uninitialize_libgdmaze);
+  init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
 
-	return init_obj.init();
+  return init_obj.init();
 }
-
 }
